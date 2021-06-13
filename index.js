648ad580072e824841e8e7b34630fcb6acf3e134
nipul1 <nipulsindwani1998@gmail.com>
2021-06-13 13:49:09 +0530

    difference between var and let declarations

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-console.log("This iss ecma script")
-var x = 20;
-console.log(x); conssolease
+var x = 10
+var y = 20
+let xx = 100
+let yy = 200
+{
+    console.log(x);
+    console.log(y);
+    console.log(xx)
+    console.log(yy);
+    let a = "nipul";
+    var b = "sindwani";
+}
+// error causing line is commented because variable a is declared using let 
+// console.log(a);
+console.log(b);
+
